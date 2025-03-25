2e0168271e0be9267173e5e554e5ae6928871de3
Justfarizz <farizmuaffaq@gmail.com>
2025-03-25 17:51:33 +0800

    Code Posttest 3

---
+++ kelas/pertemuan-4/pertemuan4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// void tambah(int a, int b)
+// {
+//     cout << a + b << endl;
+// }
+
+// int tambah(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int ubahNilai(int a = 30)
+// {
+//     return a;
+// }
+
+// int tambah(int a, int b)
+// {
+//     return a + b;
+// }
+// double tambah(double a, double b)
+// {
+//     return a + b;
+// }
+// int tambah(int a)
+// {
+//     return a + a;
+// }
+
+// int faktorial(int n) 
+// {
+//     if (n == 1);
+//         return 1;
+//     return n * faktorial(n - 1);
+// }
+
+int main()
+{
+    // cout << faktorial(5) << endl;
+
+    // cout << tambah(5, 6) << endl;
+    // cout << tambah(2.3, 4.5) << endl;
+    // cout << tambah(3) << endl;
+    return 0;
+}
